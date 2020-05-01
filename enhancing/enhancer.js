5d26c38e0457bb16ed9bc94cab16b8510a3cd6be
Nathan <nate1898@gmail.com>
2020-04-30 21:49:15 -0400

    Pushed Fix

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,29 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
-  item.enhancment != 20 ? item.enhancment += 1 : item.enhancment;
-  return { ...item };
+  let enhancment;
+
+  item.enhancment != 20 ? enhancment = item.enhancment + 1 : enhancment = item.enhancment;
+  
+  return { ...item, enhancment};
 }
 
 function fail(item) {
+  let enhancment = item.enhancment
+  let durability = item.durability
   if(item.enhancment >= 15){
-    item.enhancment > 16 ? item.enhancment -= 1 : item.enhancment;
-    item.durability -= 10;
+    item.enhancment > 16 ? enhancment = item.enhancment - 1 : enhancment = item.enhancment;
+    durability = item.durability - 10
   } else {
-    item.durability -= 5;
+    durability = item.durability - 5;
   }
 
-  item.durability < 0 ? item.durability = 0 : item.durability;
+  item.durability <= 0 ? durability = 0 : durability = durability;
 
-  return { ...item };
+  return { ...item, durability, enhancment };
 }
 
 function repair(item) {
-  item.durability = 100;
-  return { ...item };
+  let durability = 100;
+  return { ...item, durability };
 }
 
 function get(item) {
-  item.enhancment > 0 ? item.name = `[+${item.enhancment}] ${item.name}` : item.name
-  return { ...item };
+  let name;
+  item.enhancment > 0 ? name = `[+${item.enhancment}] ${item.name}` : name = item.name
+  return { ...item, name };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -10,59 +10,61 @@ describe('Testing Enhancer functions', () =>{
 
     describe('Durability functionality', () =>{
         it('should return an item with full durability', () => {
-            expect(item.durability).toBe(40);
-            enhancer.repair(item);
-            expect(item.durability).toBe(100);
+            let obj = enhancer.repair(item);
+            expect(obj.durability).toBe(100);
+            expect(item).not.toBe(obj);
         });
     })
     describe('Enhancment succeed functionality', () => {
         it('should increase enhancment', () => {
-            expect(item.enhancment).toBe(0);
-            enhancer.succeed(item);
-            expect(item.enhancment).toBe(1);
+            let obj = enhancer.succeed(item);
+            expect(obj.enhancment).toBe(1);
+            expect(item).not.toBe(obj);
         });
     
         it('should not change durability', () => {
             let durability = item.durability;
-            enhancer.succeed(item);
-            expect(item.durability).toBe(durability);
+            let obj = enhancer.succeed(item);
+            expect(obj.durability).toBe(durability);
+            expect(item).not.toBe(obj);
         });
     
         it('should not increase past 20 enhancment', () =>{
             item.enhancment = 20;
-            expect(item.enhancment).toBe(20);
-            enhancer.succeed(item);
-            expect(item.enhancment).toBe(20);
+            let obj = enhancer.succeed(item);
+            expect(obj.enhancment).toBe(20);
+            expect(item).not.toBe(obj);
         })
     });
 
     describe('Enhanment fail functionality', () => {
         it('should decrease durability by 5 if the items enhancment is less then 15 ', () => {
-            let prevDurability = item.durability;
-            for(i=0; i > 15; i++){
+            for(i=0; i < 15; i++){
                 item.enhancment = i;
-                enhancer.fail(item);
-                expect(item.durability).toBe(prevDurability - 5);
+                let obj = enhancer.fail(item);
+                expect(obj.durability).toBe(item.durability - 5);
+                expect(item).not.toBe(obj);
             }
         });
 
         it('should decrease the durability by 10 if enhancment is 15 or more', () => {
-            let prevDurability = item.durability;
-            for(i=15; i >= 20; i++){
+            for(i=15; i <= 20; i++){
                 item.enhancment = i
-                enhancer.fail(item);
-                expect(item.durability).toBe(prevDurability - 10);
+                let obj = enhancer.fail(item);
+                expect(obj.durability).toBe(item.durability - 10);
+                expect(item).not.toBe(obj);
             }
         });
 
         it('should decrease the durability by 10 and also the enhancment level by 1 if the item level is greater than 16', () => {
             let prevDurability = item.durability;
-            for(i=17; i >= 20; i++){
+            for(i=17; i <= 20; i++){
                 item.enhancment = i;
             
-                enhancer.fail(item);
-                expect(item.durability).toBe(prevDurability - 10);
-                expect(item.enhancment).toBe(i - 1);
+                let obj = enhancer.fail(item);
+                expect(obj.durability).toBe(prevDurability - 10);
+                expect(obj.enhancment).toBe(i - 1);
+                expect(item).not.toBe(obj);
             } 
         });
     });
@@ -71,16 +73,18 @@ describe('Testing Enhancer functions', () =>{
         it('should not return a name if enhancment level is 0', () => {
             item.enhancment = 0;
             let prevName = item.name
-            enhancer.get(item)
-            expect(item.name).toBe(prevName);
+            let obj = enhancer.get(item)
+            expect(obj.name).toBe(prevName);
+            expect(item).not.toBe(obj);
         });
 
         it('should Add the prefix if the enhancment level is greater then 0', () => {
             let prevName = item.name;
-            for(i=1; i >= 20; i++){
+            for(i=1; i <= 20; i++){
                 item.enhancment = i;
-                enhancer.get(item);
-                expect(item.name).toBe(`[+${item.enhancment}] ${prevName}`)
+                let obj = enhancer.get(item);
+                expect(obj.name).toBe(`[+${item.enhancment}] ${prevName}`)
+                expect(item).not.toBe(obj);
             }
         });
     })
